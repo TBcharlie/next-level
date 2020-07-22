bfbbe5e171a386534ec85c8677f777d826c560b4
tbcharlie <tbcharlie1@gmail.com>
2020-07-21 22:49:34 -0300

    3 1h 1m 33s

--- server/src/controller/PointsController.ts
+++ server/src/controller/PointsController.ts
@@ -1,57 +1,83 @@
 import Knex from '../database/connection'
-import {Request,Response} from 'express'
+import { Request, Response } from 'express'
 
 
 
-class PointsController{
-async create(request:Request,response:Response){
-  const {name,email,whatsapp,latitude,longitude,city,uf,items} = request.body;
+class PointsController {
+  async index(request: Request, response: Response) {
+  const {city,uf,items} = request.query
 
-  const trx = await Knex.transaction();
+  const parseItems = String(items)
+  .split(',')
+  .map(item => Number(item.trim()))
 
-  const point = {
-    image:'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf
+const points = await Knex('points')
+.join('point_items','points.id','=','point_items.point_id')
+.whereIn('point_items.item_id',parseItems)
+.where('city',String(city))
+.where('uf',String(uf))
+.distinct()
+.select('points.*')
+
+    return response.json({points})
   }
 
-  const insertIds = await trx('points').insert(point)
 
-  const point_id = insertIds[0]
+  async create(request: Request, response: Response) {
+    const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
 
-  const pointItems = items.map((item_id:number)=>{
-    return {
-      item_id,
-      point_id
+    const trx = await Knex.transaction();
+
+    const point = {
+      image: 'https://images.unsplash.com/photo-1550989460-0adf9ea622e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      name,
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf
     }
-  })
 
-  await trx('point_items').insert(pointItems)
+    const insertIds = await trx('points').insert(point)
 
-  return response.json({
-    id:point_id,
-    ...point,
-  })
-}
+    const point_id = insertIds[0]
+
+    const pointItems = items.map((item_id: number) => {
+      return {
+        item_id,
+        point_id
+      }
+    })
+
+    await trx('point_items').insert(pointItems)
+
+    await trx.commit()
+
+    return response.json({
+      id: point_id,
+      ...point,
+    })
+  }
 
 
-async show(request:Request,response:Response){
- const {id} = request.params
+  async show(request: Request, response: Response) {
+    const { id } = request.params
 
- const point = await Knex('points').where('id',id).first()
+    const point = await Knex('points').where('id', id).first()
 
- if(!point){
-   return response.status(400).json({message:"Point not found"})
- }
+    if (!point) {
+      return response.status(400).json({ message: "Point not found" })
+    }
 
-return response.json(point)
+    const items = await Knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title')
 
-}
+    return response.json({ point, items })
+
+  }
 }
 
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -12,6 +12,7 @@ routes.get('/', (request,response) =>{
 
 routes.get('/items',itemsController.index)
 routes.post('/points',pointsController.create)
+routes.get('/points',pointsController.index)
 routes.get('/points/:id',pointsController.show)
 
   
